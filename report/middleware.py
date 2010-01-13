b528096526f5ad00f06e4fb766ef15ea970db431
haineault <haineault@3c04d76c-ec1d-11de-bdfd-b3e7e4bb8034>
2010-01-13 02:43:56 +0000

    fixed encoding bug *sigh*

--- report/middleware.py
+++ report/middleware.py
@@ -23,48 +23,41 @@ def fetch_resources(uri, rel):
 
     """
     path = os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
-    print path
     return path
 
-import sys
+import sys, StringIO
 
 class DownloadToPDF():
     """
     download requested apge to DPF
     """
-    def __init__(self):
-        self.temp_file    = os.path.join(settings.REPORT_CONF['temp_path'], 'temp.pdf')
-        self.temp_file2   = os.path.join(settings.REPORT_CONF['temp_path'], 'temp2.pdf')
        
     def process_response(self, request, response):
-        print response.status_code
         if ("pdf" in request.GET 
             and "text" in response['Content-Type'] 
             and response.status_code == 200):
-            fd  = open(self.temp_file, 'wb')
-            pisa.showLogging()
+            fd  = StringIO.StringIO()
+#           pisa.showLogging()
             pdf = pisa.pisaDocument(
                 response.content,   # src
                 fd,                 # dest
-#               debug=0,
+                debug=0,
 #               path=None,
 #               errout=sys.stdout
 #               tempdir=None,
-                format='pdf',
-                link_callback=fetch_resources,
+                format = 'pdf',
+                link_callback = fetch_resources,
+#               encoding = 'iso-8859-1',
 #               default_css=None,
-                xhtml=True,
-#               encoding='iso-8859-1',
+#               xhtml=True,
 #               xml_output=fd,
             )
             
             fd.close()
             if not pdf.err:
                 response['Content-Type'] = u'%s; %s' % (settings.REPORT_CONF['content_type'], settings.REPORT_CONF['encoding'], )
-                fd = open(self.temp_file, 'r')
-                rs = fd.read()
+                response.content = fd.getvalue()
                 fd.close
-                response.content = rs
             return response
         else:
             return response
