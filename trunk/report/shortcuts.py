3cc5bd13a0ab01061e26a50370f120ab456b3bc3
haineault@gmail.com <haineault@gmail.com@3c04d76c-ec1d-11de-bdfd-b3e7e4bb8034>
2011-11-09 14:46:39 +0000

    Removed empty views.py

--- trunk/report/shortcuts.py
+++ trunk/report/shortcuts.py
@@ -4,14 +4,14 @@ from django.conf import settings
 from django.http import HttpResponse
 
 DELIMITER = getattr(settings, 'REPORT_DELIMITER', ';')
-QUOTECHAR = getattr(settings, 'REPORT_QUOTECHAR', '|')
-QUOTING   = getattr(settings, 'REPORT_QUOTING', csv.QUOTE_MINIMAL)
+QUOTECHAR = getattr(settings, 'REPORT_QUOTECHAR', '"')
+QUOTING   = getattr(settings, 'REPORT_QUOTING', csv.QUOTE_ALL)
 
 
 def serve_csv(request):
     if hasattr(request, 'csv_fields') and hasattr(request, 'object_list'):
         response = HttpResponse(mimetype='text/csv')
-        response['Content-Disposition'] = 'attachment; filename=somefilename.csv'
+        response['Content-Disposition'] = 'attachment; filename=%s' % getattr(request, 'csv_filename', 'Untitled.csv')
 
         writer = csv.writer(response, delimiter=DELIMITER, quotechar=QUOTECHAR, quoting=QUOTING)
 
